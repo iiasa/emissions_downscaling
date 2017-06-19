7958784757c8a250ae044fb92999c6e0d3c5478d
Leyang Feng <leyang.feng@pnnl.gov>
2017-06-19 10:50:29 -0400

    Added package 'tidyr' as needed package.

--- code/parameters/global_settings.R
+++ code/parameters/global_settings.R
@@ -19,7 +19,7 @@ loadPackage <- function( package ) {
   }
 }
 
-libs <- c( "ggplot2", "magrittr", "pbapply", "plyr", "dplyr", "reshape", "stringr", "XML", "readxl", 'zoo', 'tools')
+libs <- c( "ggplot2", "magrittr", "pbapply", "plyr", "dplyr", "reshape", "stringr", "XML", "readxl", 'zoo', 'tools', 'tidyr' )
 for( i in seq_along( libs ) ) {
     package <- libs[[ i ]]
     loadPackage( package )
