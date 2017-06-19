287b900cdcebdb0456b5372a4472b6e282852f9c
Leyang Feng <leyang.feng@pnnl.gov>
2017-06-19 11:04:12 -0400

    Updated iamInfoExtract function include parameter extraction for gridding.

--- code/parameters/all_module_functions.R
+++ code/parameters/all_module_functions.R
@@ -19,9 +19,10 @@
 iamInfoExtract <- function( master_config, iam ) {
   
   # first check whether the given iam is valid or not
+  iam <- gsub( '-', '.', iam, fixed = T )
   model_names <- colnames( master_config )[ 2 : ncol( master_config ) ]
   if ( ( iam %in% model_names ) == F ) { stop( 'The provided IAM does not have configuration information' ) } 
-  
+
   # extract the IAM configuration information  
   tango_iam_list <- as.list( master_config[ , iam ] )
   parameter_names <- master_config[ , 'model' ]
@@ -30,7 +31,7 @@ iamInfoExtract <- function( master_config, iam ) {
   # go through each element in the list 
   # and write the parameter into global environment 
   # then modifiy the list if necessary 
-  iam_name <<- iam 
+  iam_name <<- gsub( '.', '-', iam, fixed = T )  
   iam_path <<- tango_iam_list$model_datapath
   iam_domain_extension <<- tango_iam_list$iam_data_domain_extension
   iam_sheet_name <<- tango_iam_list$sheet_name
@@ -48,7 +49,8 @@ iamInfoExtract <- function( master_config, iam ) {
   base_year <<- tango_iam_list$base_year
   harm_start_year <<- tango_iam_list$harmonization_start_year
   harm_end_year <<- tango_iam_list$harmonization_end_year
-  harmonization_method_specific_flag <<- tango_iam_list$harmonization_method_specific
+  harmonization_method_specific_flag <<- tango_iam_list$harmonization_method_specific 
+  if ( is.na( harmonization_method_specific_flag ) ) harmonization_method_specific_flag <- 'N'
   if ( harmonization_method_specific_flag == 'Y' ) {
     harm_method_specific_type <<- tango_iam_list$harmonization_method_specific_type
     harm_method_specific_mapping <<- tango_iam_list$harmonization_method_specific_mapping
@@ -65,7 +67,7 @@ iamInfoExtract <- function( master_config, iam ) {
     harmonization_method_specific_type <- NA
     harmonization_method_specific_mapping <- NA
     harm_method_list <- c( 'offset_constant', 'offset_reduced', 
-                           'ratio_constant', 'ratio_reduced' )
+                           'ratio_constant', 'ratio_reduced', 'no_harmonization' )
     harm_method <<- tango_iam_list$harmonization_method 
     harm_type <<- unlist( strsplit( harm_method, '_' ) )[ 1 ]
     if ( ( harm_method %in% harm_method_list ) == F ) { 
@@ -94,19 +96,30 @@ iamInfoExtract <- function( master_config, iam ) {
   ds_start_year <<- tango_iam_list$downscaling_start_year 
   ds_end_year <<- tango_iam_list$downscaling_end_year
   if ( ds_sector_scheme == 'CEDS16' ) {
+    ds_sector_mapping <<- tango_iam_list$downscaling_sector_mapping_CEDS16
     ds_method_mapping <<- tango_iam_list$downscaling_method_mapping_CEDS16 
     iamc_var_name_mapping <<- tango_iam_list$IAMC_sector_name_mapping_CEDS16
   } else { 
+      ds_sector_mapping <<- tango_iam_list$downscaling_sector_mapping_CEDS9
       ds_method_mapping <<- tango_iam_list$downscaling_method_mapping_CEDS9
       iamc_var_name_mapping <<- tango_iam_list$IAMC_sector_name_mapping_CEDS9
       }
   # - gridding related parameters 
   gridding_sector_scheme <<- tango_iam_list$gridding_sector_scheme
   gridding_location_index <<- tango_iam_list$gridding_location_index
+  grid_resolution <<- as.numeric( tango_iam_list$gridding_resolution )
     if ( ds_sector_scheme == 'CEDS16' ) {
     gridding_sector_mapping <<- tango_iam_list$gridding_sector_mapping_CEDS16
+    gridding_proxy_path <<- tango_iam_list$gridding_proxy_path_CEDS16
+    gridding_proxy_substitution_mapping <<- tango_iam_list$gridding_proxy_substitution_mapping_CEDS16
+    gridding_seasonality_mapping <<- tango_iam_list$gridding_seasonality_mapping_CEDS16
+    gridding_proxy_mapping <<- tango_iam_list$gridding_proxy_mapping_CEDS16
   } else { 
     gridding_sector_mapping <<- tango_iam_list$gridding_sector_mapping_CEDS9
+    gridding_proxy_path <<- tango_iam_list$gridding_proxy_path_CEDS9
+    gridding_proxy_substitution_mapping <<- tango_iam_list$gridding_proxy_substitution_mapping_CEDS9
+    gridding_seasonality_mapping <<- tango_iam_list$gridding_seasonality_mapping_CEDS9
+    gridding_proxy_mapping <<- tango_iam_list$gridding_proxy_mapping_CEDS9
   }
   
   # return the list into global environment 
