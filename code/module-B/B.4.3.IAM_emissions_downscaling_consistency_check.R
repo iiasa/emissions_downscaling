26503021614e89fb05f9bcbb5a19913fe89b8fdb
Gutierrez <xavier.gutierrez@pnnl.gov>
2018-03-28 15:56:48 -0400

    Load linear/ipat data. Aggregate input/output for linear ds method

--- code/module-B/B.4.3.IAM_emissions_downscaling_consistency_check.R
+++ code/module-B/B.4.3.IAM_emissions_downscaling_consistency_check.R
@@ -3,9 +3,12 @@
 # Author(s): Xavier Gutierrez
 # Date Last Updated: Mar 28, 2017
 # Program Purpose: Check that downscaled countries, when aggregated to regions, match reference emissions.
-# Input Files: intermediate-output/[SUFFIX]/B.[IAM]_emissions_downscaled_ipat_[SUFFIX].csv
+# Input Files: 
+#              intermediate-output/[SUFFIX]/B.[IAM]_emissions_linear_[SUFFIX].csv      
+#              intermediate-output/[SUFFIX]/B.[IAM]_emissions_downscaled_linear_[SUFFIX].csv
 #              intermediate-output/[SUFFIX]/B.[IAM]_emissions_ipat_[SUFFIX].csv 
-#  or??        intermediate-output/[SUFFIX]/B.[ref_name]_baseyear_ipat_[SUFFIX].csv 
+#              intermediate-output/[SUFFIX]/B.[IAM]_emissions_ipat_[SUFFIX].csv 
+#              intermediate-output/[SUFFIX]/B.[IAM]_emissions_downscaled_ipat_[SUFFIX].csv
 # 
 # Output Files: 
 # Notes: 
@@ -46,3 +49,46 @@ if ( is.na( iam ) ) iam <- "GCAM4"
 
 MODULE_B <- "../code/module-B/"
 
+# ------------------------------------------------------------------------------
+# 1. Read IAM emissions and downscaled emissions 
+
+ds_lin_in <-  readData( domain = 'MED_OUT', file_name = paste0( 'B.', iam, '_emissions_linear', '_', RUNSUFFIX ))
+ds_lin_out <- readData( domain = 'MED_OUT', file_name = paste0( 'B.', iam, '_emissions_downscaled_linear', '_', RUNSUFFIX ))
+
+ds_ipat_in <- readData( domain = 'MED_OUT', file_name = paste0( 'B.', iam, '_emissions_ipat', '_', RUNSUFFIX ))
+ds_ipat_out <- readData( domain = 'MED_OUT', file_name = paste0( 'B.', iam, '_emissions_downscaled_ipat', '_', RUNSUFFIX ) )
+
+# -----------------------------------------------------------------------------
+# 2. Confirm region-consistency for linear downscaling
+# 
+# in order to perform the consistency check, we are collapsing the 'iso' and 
+# 'sector' columns. 
+#
+# Before downscaling, 'iso' isn't used as a uniquely-identifying key, meaning
+# values are the same for all rows of the same region, regardless of iso. 
+# To collapse the 'iso' column, we select all columns != iso and grab all 
+# distinct rows. 
+# 
+# To collapse the 'sector' column, we aggregate (sum) by all columns except sector
+# and value. 
+
+ds_lin_in.agg <- ds_lin_in %>% 
+  rename_all(funs(gsub("reg_iam_em_", "", .))) %>% 
+  gather(x_year, value, -model, -scenario, -region, -em, -sector, -harm_status, -unit, -iso) %>% 
+  select(model, scenario, region, em, sector, harm_status, unit, x_year, value) %>% 
+  distinct() %>% 
+  group_by(model, scenario, region, em, harm_status, unit, x_year) %>% 
+  summarise(value=sum(value)) %>% # aggregate over sectors
+  ungroup()
+
+ds_lin_out.agg <- ds_lin_out %>% 
+  gather(x_year, value, -model, -scenario, -region, -em, -sector, -harm_status,-unit, -iso) %>% 
+  group_by(model, scenario, region, em, sector, harm_status, unit, iso) %>% 
+  summarise(value=sum(value)) %>% # aggregate over sectors
+  ungroup()
+
+  
+
+
+# -----------------------------------------------------------------------------
+# 3. Confirm region-consistency for ipat downscaling
