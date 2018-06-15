2cccb590d25da502a6ecf16daf39b7e6c301bff8
Caleb Braun <caleb.braun@pnnl.gov>
2018-06-15 15:29:08 -0400

    Missing param pt 3

--- code/parameters/nc_generation_functions.R
+++ code/parameters/nc_generation_functions.R
@@ -744,7 +744,7 @@ add_global_atts <- function( nc_new, MD_comment, institution, institution_id,
   ncatt_put( nc_new, 0, 'source_id', MD_source_id_value )
   ncatt_put( nc_new, 0, 'source_version', dataset_version_number )
   ncatt_put( nc_new, 0, 'table_id', 'input4MIPs' )
-  ncatt_put( nc_new, 0, 'target_mip', target_mip )
+  ncatt_put( nc_new, 0, 'target_mip', target_MIP )
   ncatt_put( nc_new, 0, 'title', paste( 'Future', sector_long_name, 'of', FN_em, 'prepared for input4MIPs' ) )
   ncatt_put( nc_new, 0, 'variable_id', MD_variable_id_value )
 }
